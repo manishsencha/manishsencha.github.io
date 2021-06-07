e40c66dbea413dac373e9d549b4f250e267c6f90
Manish Sencha <manishsencha2001@gmail.com>
2021-06-07 14:59:40 +0530

    updated projects

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -14,7 +14,17 @@ const projectDetails = [
     demo: "https://manishsencha.github.io/todo-app",
     image: require("../Images/Todo-app.png").default,
     title: "Todo-App",
-    description: "The project is a basic todo app. It is simple static webpage to manage your tasks. The project is based on ReactJS with Redux state management. The components used in the project are from Material-UI: A popular React UI framework.",
+    description:
+      "The project is a basic todo app. It is simple static webpage to manage your tasks. The project is based on ReactJS with Redux state management. The components used in the project are from Material-UI: A popular React UI framework.",
+  },
+  {
+    id: 3,
+    link: "https://github.com/manishsencha/cpp-compiler",
+    demo: "https://cpp-compiler.herokuapp.com",
+    image: require("../Images/cppcompiler.png").default,
+    title: "CPP Compiler",
+    description:
+      "This is an online C++ compiler. Frontend for the project is developed using React and the editor used in the project is monaco editor which is same editor that powers VS Code Editor. Backend for the project is developed using Express framework. The API used in the project for compiling the code is used from https://godbolt.org/",
   },
 ];
 export default projectDetails;
