783af384d98d667d076fab90e22bfc6edf075358
Manish Sencha <manishsencha2001@gmail.com>
2021-06-25 16:58:43 +0530

    Increased component reusability

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,51 +7,7 @@ import {
   RadioButtonChecked,
   RadioButtonUnchecked,
 } from "@material-ui/icons";
-
-const list = [
-  {
-    title: "CPP Compiler",
-    description:
-      "This is an online C++ compiler. Frontend for the project is developed using React and the editor used in the project is monaco editor which is same editor that powers VS Code Editor. Backend for the project is developed using Express framework. The API used in the project for compiling the code is used from https://godbolt.org/",
-    github: "https://github.com/manishsencha/cpp-compiler",
-    project: "https://cpp-compiler.herokuapp.com/",
-    image: "cppcompiler.png",
-  },
-  {
-    title: "Movies Search",
-    description:
-      "This is movie search webpage. The program is developed using React. The api used in the project is OmdbApi for fetching the movies.",
-    github: "https://github.com/manishsencha/movies-search",
-    project: "https://manishsencha.github.io/movies-search",
-    image: "moviessearch.png",
-  },
-  {
-    title: "Todo App",
-    description:
-      "The project is a basic todo app. It is simple static webpage to manage your tasks. The project is based on ReactJS with Redux state management. The components used in the project are from Material-UI: A popular React UI framework.",
-    github: "https://github.com/manishsencha/todo-app",
-    project: "https://github.com/manishsencha/Todo-app",
-    image: "todoapp.png",
-  },
-  {
-    title: "Typing Test",
-    description:
-      "This is a typing test program. The project is built using HTML, CSS, JavaScript and Bootstrap. The time to take test is 60 seconds and after completing it shows result with incorrect words, correct words and accuracy.",
-    github: "https://github.com/manishsencha/typing-test",
-    project: "https://github.com/manishsencha/typing-test",
-    image: "typingtest.png",
-  },
-  {
-    title: "URL-Shortener",
-    description:
-      'This project is developed to shorten long URLs. It generates a short URL based on input long URL. The project is based on ExpressJS framework for web and mobile applications. It is also connected to mongodb database to store the URLs. An NPM package called "shortid" was used to generate short URLs.',
-    github: "https://github.com/manishsencha/URL-Shortener",
-    project: "https://m-urlshortener.herokuapp.com/",
-    image: "urlshortener.png",
-  },
-];
-
-
+import projectDetails from "./projectDetails";
 function Projects() {
   const width = useWindowWidth() - useWindowWidth() / 20;
   const height = (3 * width) / 4;
@@ -76,7 +32,7 @@ function Projects() {
       <div className="projects-heading">Projects</div>
       <div className="projects-carousel-container">
         <img
-          src={require("../../Images/" + list[slide].image).default}
+          src={require("../../Images/" + projectDetails[slide].image).default}
           style={{
             width: width,
             height: height,
@@ -84,7 +40,7 @@ function Projects() {
             maxHeight: "800px",
             position: "relative",
           }}
-          alt={list[slide]}
+          alt={projectDetails[slide]}
           className="projects-carousel-image"
         />
         <div
@@ -155,11 +111,13 @@ function Projects() {
         )}
       </div>
       <div className="projects-details">
-        <div className="project-heading">{list[slide].title}</div>
-        <div className="project-description">{list[slide].description}</div>
+        <div className="project-heading">{projectDetails[slide].title}</div>
+        <div className="project-description">
+          {projectDetails[slide].description}
+        </div>
         <div className="project-target-links">
           <a
-            href={list[slide].github}
+            href={projectDetails[slide].github}
             target="_blank"
             className="project-target-link"
             rel="noreferrer"
@@ -167,7 +125,7 @@ function Projects() {
             GITHUB
           </a>
           <a
-            href={list[slide].project}
+            href={projectDetails[slide].project}
             className="project-target-link"
             target="_blank"
             rel="noreferrer"
