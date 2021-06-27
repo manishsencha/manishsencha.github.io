b73d3d65e4aecae76eec3020c959dbec42d39541
Manish Sencha <manishsencha2001@gmail.com>
2021-06-27 17:19:59 +0530

    added a preloader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
+import useWindowWidth from "./utils/windowWidth";
+import useWindowHeight from "./utils/windowHeight";
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+  const height = useWindowHeight();
+  const width = useWindowWidth();
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  });
+
+  return loading ? (
+    <div
+      style={{
+        height: height,
+        width: width,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Loading
+    </div>
+  ) : (
     <>
       <Header />
       <Home />
