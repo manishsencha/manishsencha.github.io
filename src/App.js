98b5bbc928b6d8d5f72657d6135f94fddeebf3fd
Manish Sencha <manishsencha2001@gmail.com>
2021-07-08 15:04:47 +0530

    UI improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
@@ -8,7 +6,7 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-
+import './App.css'
 function App() {
   const [loading, setLoading] = useState(true);
   const [display, setDisplay] = useState("flex")
@@ -20,7 +18,7 @@ function App() {
   }, [])
   return (<div className="my-app">
     <div style={{ transition: "opacity 1s ease-in-out", position: "absolute", height: "100%", width: "100%", backgroundColor: "white", opacity: loading ? 1 : 0, display: display, justifyContent: "center", alignItems: "center" }}>
-      <Loader type="Grid" color="rgb(var(--primary-color))" />
+      <div className="loader">Loading....</div>
     </div>
     <div style={{ height: loading ? 0 : "", overflow: loading ? "hidden" : "" }}>
       <Header />
