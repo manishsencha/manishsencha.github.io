aafba5d47bb8519fe1b0c135691ffd774dff30e4
Manish <manishsencha2001@gmail.com>
2023-05-25 21:27:28 +0530

    Added loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { RingLoader } from "react-spinners";
 
 const Loader: React.FC = () => {
   return (
     <div className="min-vh flex items-center justify-center">
-      <div className="">Loader</div>
+      <RingLoader className="loading-spinner"/>
     </div>
   );
 };
